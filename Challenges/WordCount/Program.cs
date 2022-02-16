98a833f12958031010ca15b6b8c202a61fb9735d
bBekan <69426942+bBekan@users.noreply.github.com>
2022-02-16 17:57:14 +0100

    Changed WordCount

--- Challenges/WordCount/Program.cs
+++ Challenges/WordCount/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.RegularExpressions;
+using WordCount;
 
 string[] tests = new string[]
 {
@@ -13,6 +14,31 @@ excluding whitespace (including line returns), and the
 number of words in the string. Finally, list each word, ensuring it
 is a valid word."
 };
+
+
+foreach(var s in tests)
+{
+    try
+    {
+        var textAnalyzer = new TextAnalyzer(s);
+
+        var mostUsedWord = textAnalyzer.MostUsedWord;
+        var mostUsedChar = textAnalyzer.MostUsedChar;
+
+        Console.WriteLine("Total words: {0}", textAnalyzer.NumberOfWords);
+        Console.WriteLine("Total Characters: {0}", textAnalyzer.NumberOfCharacters);
+        Console.WriteLine("Character count (minus line returns and spaces): {0}", textAnalyzer.NumberOfAsciiCharacters);
+        Console.WriteLine("Most used word: {0} ({1} times)", mostUsedWord, textAnalyzer.Words[mostUsedWord]);
+        Console.WriteLine("Most used word: {0} ({1} times)", mostUsedChar, textAnalyzer.Characters[mostUsedChar]);
+        Console.WriteLine();
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    
+
+}
 
 /* 
     First string (tests[0]) Values:
@@ -29,47 +55,3 @@ is a valid word."
     Most used word: the (6 times)
     Most used character: t (24 times)
 */
-Dictionary<string, int> words = new Dictionary<string, int>();
-Dictionary<char, int> chars = new Dictionary<char, int>();
-char[] delims = new[] { '\r', '\n',  ' ' };
-
-foreach(var s in tests)
-{
-    string[] wordArray = s.Split(delims, StringSplitOptions.RemoveEmptyEntries);
-    Console.WriteLine("Total words: {0}", wordArray.Length);
-    Console.WriteLine("Total Characters: {0}",s.Replace("\n", "").Length);
-    Console.WriteLine("Character count (minus line returns and spaces): {0}", string.Join(null, wordArray).ToCharArray().Length);
-    foreach (var z in wordArray)
-    {
-        var alphaString = Regex.Replace(z.ToLower(), "[^A-Za-z0-9 -]", "");
-        if (words.Keys.Contains(alphaString))
-        {
-            words[alphaString]++;
-        }
-        else
-        {
-            words.Add(alphaString, 1);
-        }
-        foreach(var c in alphaString)
-        {
-            if (chars.Keys.Contains(c))
-            {
-                chars[c]++;
-            }
-            else
-            {
-                chars.Add(c, 1);
-            }
-        }
-    }
-
-    var maxWords = words.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-    Console.WriteLine("Most used word: {0} ({1} times)", maxWords, words[maxWords]);
-
-    var maxChars = chars.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-    Console.WriteLine("Most used character: {0} ({1} times)", maxChars, chars[maxChars]);
-
-    words.Clear();
-    chars.Clear();
-    Console.WriteLine();
-}
---
+++ Challenges/WordCount/TextAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    internal class TextAnalyzer
+    {
+        public Dictionary<string, int> Words { get; private set; }
+        public int NumberOfWords { get; private set; }
+        public int NumberOfCharacters { get; private set; }
+        public int NumberOfAsciiCharacters { get; private set; }
+        public string MostUsedWord { get; private set; }
+        public char MostUsedChar { get; set; }
+        public Dictionary<char, int> Characters { get; private set; }
+
+        public TextAnalyzer(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) throw new Exception("Text cannot be empty!");
+
+            Words = new Dictionary<string, int>();
+            Characters =  new Dictionary<char, int>();
+
+            AnalyzeText(text);
+        }
+
+        private void AnalyzeText(string text)
+        {
+            char[] delims = new[] { '\r', '\n', ' ' };
+            string[] words = text.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+
+            NumberOfCharacters = text.Replace("\n", "").Length;
+            NumberOfAsciiCharacters = string.Join(null, words).ToCharArray().Length;
+
+            foreach (var w in words)
+            {
+                var alphaString = Regex.Replace(w.ToLower(), "[^a-z -]", "");
+                if (Words.Keys.Contains(alphaString))
+                {
+                    Words[alphaString]++;
+                }
+                else
+                {
+                    Words.Add(alphaString, 1);
+                }
+                foreach (var c in alphaString)
+                {
+                    if (Characters.Keys.Contains(c))
+                    {
+                        Characters[c]++;
+                    }
+                    else
+                    {
+                        Characters.Add(c, 1);
+                    }
+                }
+            }
+            NumberOfWords = Words.Keys.Count;
+            MostUsedWord = GetMostUsedKey<string>(Words);
+            MostUsedChar = GetMostUsedKey<char>(Characters);
+        }
+
+        private T GetMostUsedKey<T>(Dictionary<T, int> entries)
+        {
+            return entries.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+        }
+    }
+
+}
