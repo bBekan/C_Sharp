5e228d10f6d6b82d6ddb7cb9c2851d1b43e4d568
bBekan <69426942+bBekan@users.noreply.github.com>
2022-03-09 19:08:09 +0100

    DatabaseIndexing

--- MVC/Vidly/Migrations/20220307155135_FillMembershipTypes.cs
+++ MVC/Vidly/Migrations/20220307155135_FillMembershipTypes.cs
@@ -8,10 +8,10 @@ namespace Vidly.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (0,'Monthly', 15, 1, 0)");
-            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (1,'Quarterly', 5, 3, 0)");
-            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (2,'Half a year', 0, 6, 5)");
-            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (3,'Yearly', 0, 12, 15)");
+            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (1,'Monthly', 15, 1, 0)");
+            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (2,'Quarterly', 5, 3, 0)");
+            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (3,'Half a year', 0, 6, 5)");
+            migrationBuilder.Sql("INSERT INTO MEMBERSHIPS (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (4,'Yearly', 0, 12, 15)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
--- MVC/Vidly/Migrations/20220307162856_PopulateGenres.cs
+++ MVC/Vidly/Migrations/20220307162856_PopulateGenres.cs
@@ -8,17 +8,17 @@ namespace Vidly.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (0,'Action')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (1,'Comedy')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (2,'Horror')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (3,'Romance')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (4,'Drama')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (5,'Thriller')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (6,'Musicle')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (7,'Adventure')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (8,'Sci-fi')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (9,'Fantasy')");
-            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (10,'Documentary')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (1,'Action')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (2,'Comedy')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (3,'Horror')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (4,'Romance')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (5,'Drama')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (6,'Thriller')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (7,'Musicle')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (8,'Adventure')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (9,'Sci-fi')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (10,'Fantasy')");
+            migrationBuilder.Sql("INSERT INTO Genres (Id, Name) VALUES (11,'Documentary')");
 
         }
 
